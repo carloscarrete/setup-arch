43f29840269f2b1c318191bbf49505ac2758afe3
Carlos Carrete <carretecarlosg@gmail.com>
2022-11-27 00:47:53 -0600

    First version?

--- script.sh
+++ script.sh
@@ -1,2 +1,14 @@
-#!bin/bash
+#/!bin/bash
+
 pacstrap -K /mnt base linux linux-firmware nano git
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Mexico/General /etc/localtime
+hwclock --systohc
+git clone https://gitlab.com/carloscarrete.sc/arch-files
+cd arch-files
+mv ./locale.gen /etc/locale.gen
+locale-gen
+mv ./locale.conf /etc/locale.conf
+mv ./vconsole.conf /etc/vconsole.conf
+pacman -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers git bluez bluez-utils pulseaudio-bluetooth cups openssh
