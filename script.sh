7d7ff63d84c6d958c337183c17f2510f295ed743
Carlos Carrete <carretecarlosg@gmail.com>
2022-11-28 23:32:11 -0600

    pre-install v1

--- script.sh
+++ script.sh
@@ -1,8 +1,4 @@
 #/!bin/bash
-
-pacstrap -K /mnt base linux linux-firmware nano git
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Mexico/General /etc/localtime
 hwclock --systohc
 git clone https://gitlab.com/carloscarrete.sc/arch-files
@@ -11,4 +7,6 @@ mv ./locale.gen /etc/locale.gen
 locale-gen
 mv ./locale.conf /etc/locale.conf
 mv ./vconsole.conf /etc/vconsole.conf
+echo carlos >> /etc/hostname
 pacman -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers git bluez bluez-utils pulseaudio-bluetooth cups openssh
+#pacman -S grub efibootmgr os-prober ntfs-3g networkmanager network-manager-applet wireless_tools wpa_supplicant dialog mtools dosfstools base-devel linux-headers git bluez bluez-utils pulseaudio-bluetooth cups openssh
