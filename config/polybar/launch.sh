d2d75c67098ffdc138519b87539a49473d9c6aef
Carlos <carretecarlosg@gmail.com>
2022-12-29 21:28:24 +0000

    important changes

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -9,3 +9,8 @@ killall -q polybar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
 echo "Bars launched..."
+
+
+if [[ $(xrandr -q | grep 'HDMI1 connected') ]]; then
+  polybar second &
+fi
